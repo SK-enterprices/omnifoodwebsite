c449a309dbc8b51481815b1f865282d608838006
sangameshsk <sangameshsk210@gmail.com>
2023-03-12 12:48:56 +0530

    first ever commit

---
+++ js/script1.js
@@ -0,0 +1,89 @@
+console.log("hello world!");
+
+const myName = "Sangamesh kajagar";
+console.log(myName);
+
+// set current year
+
+const yearEL = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEL.textContent = currentYear;
+
+//  make mobile navigation work
+const batNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+batNavEl.addEventListener("click", function() {
+    headerEl.classList.toggle("nav-open");
+});
+
+//  Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function(link) {
+    link.addEventListener("click", function(e) {
+        e.preventDefault();
+        const href = link.getAttribute("href");
+        // console.log(href);
+
+        //
+
+        if (href === "#")
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
+
+        // close mobile nav
+
+        if (href !== "#" && href.startsWith("#")) {
+            const sectionEl = document.querySelector(href);
+            sectionEl.scrollIntoView({ behavior: "smooth" });
+        }
+        if (link.classList.contains("main-nav-link")) {
+            headerEl.classList.toggle("nav-open");
+        }
+    });
+});
+
+// ////////////////////////////////////
+//  sticky nav
+const sectionHeroel = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+    function(entries) {
+        const ent = entries[0];
+        console.log(ent);
+
+        if (ent.isIntersecting === false) {
+            document.body.classList.add("sticky");
+        }
+        if (ent.isIntersecting === true) {
+            document.body.classList.remove("sticky");
+        }
+    }, {
+        root: null,
+        threshold: 0,
+        rootMargin: "-80px",
+    }
+);
+obs.observe(sectionHeroel);
+
+// /////////////////////////
+
+function checkFlexGap() {
+    var flex = document.createElement("div");
+    flex.style.display = "flex";
+    flex.style.flexDirection = "column";
+    flex.style.rowGap = "1px";
+
+    flex.appendChild(document.createElement("div"));
+    flex.appendChild(document.createElement("div"));
+
+    document.body.appendChild(flex);
+    var isSupported = flex.scrollHeight === 1;
+    flex.parentNode.removeChild(flex);
+    console.log(isSupported);
+
+    if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
